2a7760e897ab2e011d9616d7e42e0a4b4d705574
Josh Martin <joshmartin5001@gmail.com>
2017-10-21 14:16:46 -0400

    rekt

---
+++ netxtern/Array.h
@@ -0,0 +1,136 @@
+
+// Honor Pledge:
+ //
+// I pledge that I have neither given nor
+//  received any help on this assignment.
+//
+//chajmart
+
+#ifndef _ARRAY_H_
+#define _ARRAY_H_
+
+#include <cstring>          // for size_t definition
+#include <iostream>
+#include "BaseArray.h"
+// Ryan: A better approach here would be to create a Base Array class
+// and inherit from it. This will eliminate the problem in which the
+// Fixed Array can inherit the resize function of this class.
+
+/**
+ * @class Array
+ *
+ * Basic implementation of a standard array class for chars.
+ */
+template <typename T>
+class Array : public BaseArray <T>
+{
+public:
+  /// Type definition of the element type.
+  typedef T type;
+
+  /// Default constructor.
+  Array (void);
+
+  /**
+   * Initializing constructor.
+   *
+   * @param[in]      length        Initial size
+   */
+  Array (size_t length);
+
+  /**
+   * Initializing constructor.
+   *
+   * @param[in]      length        Initial size
+   * @param[in]      fill          Initial value for each element
+   */
+  Array (size_t length, T fill);
+
+  /**
+   * Copy constructor
+   *
+   * @param[in]     arr         The source array.
+   */
+  Array (const Array & arr);
+
+  /// Destructor.
+  ~Array (void);
+
+  void resize (size_t new_size);
+
+  size_t size (void) const;
+/**
+   * Assignment operation
+   *
+   * @param[in]       rhs      Right-hand side of equal sign
+   * @return          Reference to self
+   */
+  const Array & operator = (const Array & rhs);
+  /**
+   * Test the array for equality.
+   *
+   * @param[in]       rhs                  Right hand side of equal to sign
+   * @retval          true                 Left side is equal to right side
+   * @retval          false                Left side is not equal to right side
+   */
+  bool operator == (const Array & rhs) const;
+
+  /**
+   * Test the array for inequality.
+   *
+   * @param[in]       rhs                  Right-hand size of not equal to sign
+   * @retval          true                 Left side is not equal to right side
+   * @retval          false                Left size is equal to right side
+   */
+  bool operator != (const Array & rhs) const;
+  /**
+   * Retrieve the maximum size of the BaseArray.
+   *
+   * @return          The maximum size
+   */
+  size_t max_size (void) const;
+
+  /**
+   * Get the character at the specified index. If the index is not
+   * within the range of the array, then std::out_of_range exception
+   * is thrown.
+   *
+   * @param[in]       index               Zero-based location
+   * @exception       std::out_of_range   Invalid \a index value
+   */
+  T & operator [] (size_t index);
+
+  /**
+   * @overload
+   *
+   * The returned character is not modifiable.
+   */
+  const T & operator [] (size_t index) const;
+
+  /**
+   * Get the character at the specified index. If the \a index is not within
+   * the range of the BaseArray, then std::out_of_range exception is thrown.
+   *
+   * @param[in]       index                 Zero-based location
+   * @return          Character at \index
+   * @exception       std::out_of_range     Invalid index value
+   */
+  T get (size_t index) const;
+
+  /**
+   * Set the character at the specified \a index. If the \a index is not
+   * within range of the BaseArray, then std::out_of_range exception is
+   * thrown.
+   *
+   * @param[in]       index                 Zero-based location
+   * @param[in]       value                 New value for character
+   * @exception       std::out_of_range     Invalid \a index value
+   */
+  void set (size_t index, T value);
+
+};
+
+#include "Array.inl"
+#include "Array.cpp"
+
+#endif   // !defined _ARRAY_H_
